06ee4b4f47bf0bedf81045e3f378c01a59e96de1
ekrambd <ekramhossainekram28@gmail.com>
2025-10-17 12:40:21 +0600

    lt 121 modified

--- leetcode_121.js
+++ leetcode_121.js
@@ -3,7 +3,7 @@
 var maxProfit = function(prices) {
     let buy = prices[0];
     let maxProfit = 0;
-    for(let i = 0; i < prices.length; i++){
+    for(let i = 1; i < prices.length; i++){
         if(prices[i] < buy){
             buy = prices[i];
         }else{
